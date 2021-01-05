a8ffabb60634f3c2ac45a3a33085ce2125ec30bb
Wijaya <wijayaac25@gmail.com>
2021-01-05 14:27:48 +0800

    layouting components in gatsby

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -6,6 +6,7 @@ const blog = () => {
     return (
         <Layout>
             <h1>This is our blog</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
         </Layout>
     )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,11 @@
 import React from 'react'
-import Layout from '../components/layout';
+import Layout from '../components/layout'
 
 const products = () => {
     return (
         <Layout>
             <h1>This is our products</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
         </Layout>
     )
 }
