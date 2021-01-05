41fcff5bffcb70999c777dbfbe46930ce1cdd337
Wijaya <wijayaac25@gmail.com>
2021-01-05 14:48:36 +0800

    css modules in gatsby

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import Layout from '../components/layout';
+import styles from '../components/blog.module.css';
 
 
 const blog = () => {
     return (
-        <Layout>
-            <h1>This is our blog</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+        <Layout >
+            <div className={styles.page} >
+                <h1>This is our blog</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+            </div>
         </Layout>
     )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Layout from '../components/layout'
+import styles from '../components/products.module.css';
 
 const products = () => {
     return (
         <Layout>
-            <h1>This is our products</h1>
-            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+            <div className={styles.page}>
+                <h1>This is our products</h1>
+                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+            </div>
         </Layout>
     )
 }
