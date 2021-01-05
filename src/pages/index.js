d2cbe160cc352a485b262b4748d55e57fe0bd471
Wijaya <wijayaac25@gmail.com>
2021-01-05 13:49:20 +0800

    pages in gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
 import React from "react"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <div>
+    <h1>Hello people</h1>
+    <a href="/blog/">Go to blog page</a><br />
+    <a href="https://www.gatsbyjs.com" >Read the docs here</a>
+  </div>
 }
