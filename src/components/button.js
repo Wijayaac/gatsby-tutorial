50cbe2dab7f0af75eb4768bf165ba1546e25685a
Wijaya <wijayaac25@gmail.com>
2021-01-05 15:10:41 +0800

    styled components in gatsby

---
+++ src/components/button.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+export const ExampleButton = styled.button`
+background-color: blue;
+border-radius:3px;
+border: 1px solid blue;
+color: white;
+font-size: 1.5rem;
+text-transform: capitalize;
+`
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import React from "react"
 import Layout from '../components/layout';
+import { ExampleButton } from "../components/button";
 
 export default () => (
 
   <Layout>
     <h1>Hello from main page</h1>
     <p style={{ color: "grey" }} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+    <ExampleButton> Click Me !!</ExampleButton>
   </Layout>
 
 )
