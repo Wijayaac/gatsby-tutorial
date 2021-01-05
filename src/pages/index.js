0a86dc430fda6d860763677b2739c75b0e89de23
Wijaya <wijayaac25@gmail.com>
2021-01-05 14:28:19 +0800

    inline css in gatsby, same like react

--- src/pages/index.js
+++ src/pages/index.js
@@ -5,7 +5,7 @@ export default () => (
 
   <Layout>
     <h1>Hello from main page</h1>
-    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
+    <p style={{ color: "grey" }} >Lorem ipsum dolor sit amet consectetur, adipisicing elit. Explicabo incidunt beatae dolore porro? Voluptatem quod pariatur reprehenderit numquam excepturi, saepe veritatis natus repellendus ullam eos!</p>
   </Layout>
 
 )
