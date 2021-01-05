95b53023bb14245cc082e28cc5c9bccf5a4abb2c
Wijaya <wijayaac25@gmail.com>
2021-01-05 14:01:05 +0800

    components in gatsby

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,10 @@
 import React from "react"
 import { Link } from 'gatsby'
-
+import Navbar from '../components/Navbar';
 
 export default function Home() {
   return <div>
+    <Navbar />
     <h1>Hello people</h1>
     <Link to="/blog/">Go to blog page</Link><br />
     <a href="https://www.gatsbyjs.com" >Read the docs here</a>
